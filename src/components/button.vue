dd4237ba02436e832cd0bdc3b1e3151fe94f1657
Vladimir Kharlampidi <nolimits4web@gmail.com>
2016-10-19 22:22:59 +0300

    Button using mixin

--- src/components/button.vue
+++ src/components/button.vue
@@ -1,37 +1,32 @@
-<template>
-  <a :href="link && (typeof link === 'string' ? link : '#')" class="button" :class="classObject"><slot></slot></a>
-</template>
 <script>
+  import LinkMixin from '../mixins/link.vue';
   export default {
-    props: {
-      'text': String,
-      'link': [String, Boolean],
-      'external': Boolean,
-      'link-external': Boolean,
-
-      'color': String,
-      'ripple-color': String,
-
-      'round': Boolean,
-      'fill': Boolean,
-      'big': Boolean,
-      'raised': Boolean,
-
-      'active': Boolean
-    },
-    computed: {
-        classObject: function () {
-            var self = this;
-            var co = {
-                active: self.active,
-            };
-            ['round', 'fill', 'big', 'raised'].forEach(function (prop, index) {
-                if (self[prop]) co['button-' + prop] = true;
-            });
-            if (self.color) co['color-' + self.color] = true;
-            if (self.rippleColor) co['ripple-' + self.rippleColor] = true;
-            return co;
+    render: function (c) {
+      var iconEl, textEl, self = this;
+      if (self.text) {
+        textEl = c('span', {}, self.text);
+      }
+      if (self.icon) {
+        var iconClass = {'icon': true};
+        iconClass[self.icon] = true;
+        iconEl = c('i', {class:iconClass})
+      }
+      self.classesObject['button'] = true;
+      var linkEl = c('a', {
+        class: self.classesObject,
+        attrs: self.attrsObject,
+        on: {
+          click: self.onClick
         }
+      }, [iconEl, textEl, self.$slots.default]);
+
+      return linkEl;
+    },
+    mixins: [LinkMixin],
+    methods: {
+      onClick: function (event) {
+        this.$emit('click', event);
+      }
     }
   }
 </script>
