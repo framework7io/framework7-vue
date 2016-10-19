0b778cf1c23cc0219ff93bab198888537a7870a2
Vladimir Kharlampidi <nolimits4web@gmail.com>
2016-10-19 22:23:29 +0300

    Kitchen Sink begin

--- src/pages/contacts.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <f7-page name="contacts">
-    <p>Contacts Page</p>
-    <p><a href="#" class="back">Go Back</a></p>
-  </f7-page>
-</template>
-<script>
-  export default {}
-</script>
---
+++ src/pages/content-block.vue
@@ -0,0 +1,21 @@
+<template>
+  <f7-page name="about">
+    <f7-navbar>
+      <f7-nav-left sliding back-link="Back"></f7-nav-left>
+      <f7-nav-center sliding>Content Block</f7-nav-center>
+    </f7-navbar>
+    <f7-content-block>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu felis volutpat, rutrum ex quis, lobortis ex. Curabitur quis mattis lorem. Nullam magna lacus, interdum vel maximus nec, vestibulum non quam. Phasellus ornare efficitur porttitor. Quisque neque diam, imperdiet in fermentum nec, congue vitae ante. Nullam imperdiet maximus commodo. Morbi pharetra id purus ac ultrices. Duis non posuere libero.</p>
+    </f7-content-block>
+    <f7-content-block inner>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu felis volutpat, rutrum ex quis, lobortis ex. Curabitur quis mattis lorem. Nullam magna lacus, interdum vel maximus nec, vestibulum non quam. Phasellus ornare efficitur porttitor. Quisque neque diam, imperdiet in fermentum nec, congue vitae ante. Nullam imperdiet maximus commodo. Morbi pharetra id purus ac ultrices. Duis non posuere libero.</p>
+    </f7-content-block>
+    <f7-content-block inner inset>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu felis volutpat, rutrum ex quis, lobortis ex. Curabitur quis mattis lorem. Nullam magna lacus, interdum vel maximus nec, vestibulum non quam. Phasellus ornare efficitur porttitor. Quisque neque diam, imperdiet in fermentum nec, congue vitae ante. Nullam imperdiet maximus commodo. Morbi pharetra id purus ac ultrices. Duis non posuere libero.</p>
+    </f7-content-block>
+
+  </f7-page>
+</template>
+<script>
+  export default {}
+</script>
