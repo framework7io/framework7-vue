952123ee856ee12eb5ec3461d4e53ab95cd99323
Vladimir Kharlampidi <nolimits4web@gmail.com>
2016-10-20 23:46:00 +0300

    No link text in material nav left

--- src/components/nav-left.vue
+++ src/components/nav-left.vue
@@ -2,13 +2,20 @@
   <div class="left" :class="{sliding:sliding}">
     <a href="#" class="back link" v-if="backLink" :class="{'icon-only': backLink === true}">
       <i class="icon icon-back"></i>
-      <span v-if="backLink !== true">{{backLink}}</span>
+      <span v-if="backLink !== true && !material">{{backLink}}</span>
     </a>
     <slot></slot>
   </div>
 </template>
 <script>
   export default {
+    computed: {
+      material: function () {
+        var mt = false;
+        if ((this.$f7 && this.$f7.params.material) || (this.$root.$options.framework7 && this.$root.$options.framework7.material)) mt = true;
+        return mt;
+      }
+    },
     props: {
       backLink: [Boolean, String],
       sliding: Boolean
