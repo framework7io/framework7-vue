e0032c5c85e21fd3fffc0396e56d6682951aadfd
Vladimir Kharlampidi <nolimits4web@gmail.com>
2016-10-21 00:15:26 +0300

    Basic tabs

---
+++ src/components/tab.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="tab" :class="active ? 'active' : false">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      'active': Boolean
+    }
+  }
+</script>
---
+++ src/pages/tabs.vue
@@ -0,0 +1,24 @@
+<template>
+  <f7-page with-subnavbar>
+    <f7-navbar back-link="Back" title="Tabs" sliding>
+      <f7-subnavbar sliding>
+        <f7-buttons>
+          <f7-button tab-link="#tab1" active>Tab 1</f7-button>
+          <f7-button tab-link="#tab2">Tab 2</f7-button>
+          <f7-button tab-link="#tab3">Tab 3</f7-button>
+          <f7-button tab-link="#tab4">Tab 4</f7-button>
+        </f7-buttons>
+      </f7-subnavbar>
+    </f7-navbar>
+
+    <f7-tabs animated>
+      <f7-tab id="tab1" active>Tab 1</f7-tab>
+      <f7-tab id="tab2">Tab 2</f7-tab>
+      <f7-tab id="tab3">Tab 3</f7-tab>
+      <f7-tab id="tab4">Tab 4</f7-tab>
+    </f7-tabs>
+  </f7-page>
+</template>
+<script>
+  export default {}
+</script>
