747ff500d081a327124db6ae1670194aaad2bd15
Vladimir Kharlampidi <nolimits4web@gmail.com>
2016-10-23 18:11:50 +0300

    Icon component

---
+++ src/components/icon.vue
@@ -0,0 +1,27 @@
+<template>
+  <i class="icon" :class="classesObject">{{material}}<slot></slot></i>
+</template>
+<script>
+  export default {
+    props: {
+      'color': String,
+      'material': String, //Material Icons
+      'ion': String, //Ionicons
+      'fa': String, //Font Awesome
+      'icon': String, //Custom
+    },
+    computed: {
+      classesObject: function () {
+        var co = {
+          'material-icons': this.material,
+          'fa': this.fa
+        };
+        if (this.color) co['color-' + this.color] = true;
+        if (this.ion) co['ion-' + this.ion] = true;
+        if (this.fa) co['fa-' + this.fa] = true;
+        if (this.icon) co[this.icon] = true;
+        return co;
+      }
+    }
+  }
+</script>
