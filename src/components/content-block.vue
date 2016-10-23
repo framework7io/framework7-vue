92fe5b52b863c5033210f01d3443225ddb184c14
Vladimir Kharlampidi <nolimits4web@gmail.com>
2016-10-23 18:12:23 +0300

    Additional classes for tabs

--- src/components/content-block.vue
+++ src/components/content-block.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="content-block" :class="{'inset': inset}">
+  <div class="content-block" :class="classesObject">
     <div class="content-block-inner" v-if="inner">
       <slot>
     </div>
@@ -10,7 +10,19 @@
   export default {
     props: {
       'inset': Boolean,
-      'inner': Boolean
+      'inner': Boolean,
+      'tabs': Boolean,
+      'tab': Boolean
+    },
+    computed: {
+      classesObject: function () {
+        return {
+          'inset': this.inset,
+          'tabs': this.tabs,
+          'tab': this.tab,
+          'active': this.active
+        }
+      }
     }
   }
 </script>
