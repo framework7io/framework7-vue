611708708c06f5e999d00730615f5aea5804e1de
Vladimir Kharlampidi <nolimits4web@gmail.com>
2016-10-23 18:12:41 +0300

    Tabbar

--- src/components/toolbar.vue
+++ src/components/toolbar.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="toolbar">
+  <div class="toolbar" :class="classesObject">
     <div class="toolbar-inner">
       <slot></slot>
     </div>
   </div>
 </template>
 <script>
-  export default {}
+  export default {
+    props: {
+        bottom: Boolean,
+        tabbar: Boolean,
+        labels: Boolean,
+        scrollable: Boolean
+    },
+    computed: {
+        classesObject: function () {
+            return {
+                'toolbar-bottom': this.bottom,
+                'tabbar': this.tabbar,
+                'tabbar-labels': this.labels,
+                'tabbar-scrollabel': this.scrollable,
+            }
+        }
+    }
+  }
 </script>
