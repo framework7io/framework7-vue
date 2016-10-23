321d83e9b33d9ccb6084ced7cc1c75978a79496d
Vladimir Kharlampidi <nolimits4web@gmail.com>
2016-10-23 18:44:24 +0300

    Popup

---
+++ src/components/popup.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="popup"
+    :class="tabletFullscreen ? 'tablet-fullscreen' : false"
+    @open="onOpen"
+    @opened="onOpened"
+    @close="onClose"
+    @closed="onClosed"
+  >
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      'tablet-fullscreen': Boolean
+    },
+    methods: {
+      onOpen: function (event) {
+        this.$emit('open', event);
+      },
+      onOpened: function (event) {
+        this.$emit('opened', event);
+      },
+      onClose: function (event) {
+        this.$emit('close', event);
+      },
+      onClosed: function (event) {
+        this.$emit('closed', event);
+      },
+    }
+  }
+</script>
