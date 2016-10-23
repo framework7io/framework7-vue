7503e695ddb5d4f505d9777e5c4952c24bd916c6
Vladimir Kharlampidi <nolimits4web@gmail.com>
2016-10-23 18:41:44 +0300

    Popover

---
+++ src/components/popover.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="popover"
+    @open="onOpen"
+    @opened="onOpened"
+    @close="onClose"
+    @closed="onClosed"
+  >
+    <div class="popover-angle"></div>
+    <div class="popover-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    methods: {
+      onOpen: function (event) {
+        this.$emit('open', event);
+      },
+      onOpened: function (event) {
+        this.$emit('opened', event);
+      },
+      onClose: function (event) {
+        this.$emit('close', event);
+      },
+      onClosed: function (event) {
+        this.$emit('closed', event);
+      },
+    }
+  }
+</script>
