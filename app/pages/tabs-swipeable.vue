ab0d7e29d6c799c92ef512d9ac4bc4a26640ec6d
Vladimir Kharlampidi <nolimits4web@gmail.com>
2016-10-24 18:41:00 +0300

    Better tabs demos

--- app/pages/tabs-swipeable.vue
+++ app/pages/tabs-swipeable.vue
@@ -1,6 +1,6 @@
 <template>
-  <f7-page with-subnavbar>
-    <f7-navbar back-link="Back" title="Tabs" sliding>
+  <f7-page with-subnavbar no-page-content>
+    <f7-navbar back-link="Back" title="Tabs Swipeable" sliding>
       <f7-subnavbar sliding>
         <f7-buttons>
           <f7-button tab-link="#tab1" active>Tab 1</f7-button>
@@ -11,8 +11,8 @@
       </f7-subnavbar>
     </f7-navbar>
 
-    <f7-tabs>
-      <f7-tab id="tab1" active>
+    <f7-tabs swipeable>
+      <f7-page-content id="tab1" tab active>
         <f7-block>
           <p>Tab 1</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure odio dolor, soluta reprehenderit, corporis officia earum corrupti amet ea aspernatur praesentium, distinctio ipsa atque officiis. Recusandae ab error, atque natus.</p>
@@ -21,9 +21,9 @@
           <p>Autem ullam tempore suscipit consectetur, odit soluta. Neque quasi mollitia culpa autem, quis ab nostrum non distinctio illo magni perspiciatis et consequuntur possimus aliquam reprehenderit reiciendis praesentium consectetur, voluptas obcaecati?</p>
           <p>Quae repellendus aut iste provident consectetur inventore voluptatibus, saepe ad dolorem voluptatum recusandae praesentium animi harum officiis error nulla ullam nobis? Mollitia possimus voluptatum pariatur omnis quibusdam, quidem recusandae velit.</p>
         </f7-block>
-      </f7-tab>
-      <f7-tab id="tab2">
-        <f7-block>
+      </f7-page-content>
+      <f7-page-content id="tab2" tab>
+         <f7-block>
           <p>Tab 2</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure odio dolor, soluta reprehenderit, corporis officia earum corrupti amet ea aspernatur praesentium, distinctio ipsa atque officiis. Recusandae ab error, atque natus.</p>
           <p>Quibusdam repellendus, repudiandae nulla commodi ut nemo eum, quia dolorum dicta voluptate reprehenderit. Omnis, temporibus iure quia ad sit incidunt similique et quaerat dicta delectus aspernatur, unde illo cum ipsam?</p>
@@ -31,9 +31,9 @@
           <p>Autem ullam tempore suscipit consectetur, odit soluta. Neque quasi mollitia culpa autem, quis ab nostrum non distinctio illo magni perspiciatis et consequuntur possimus aliquam reprehenderit reiciendis praesentium consectetur, voluptas obcaecati?</p>
           <p>Quae repellendus aut iste provident consectetur inventore voluptatibus, saepe ad dolorem voluptatum recusandae praesentium animi harum officiis error nulla ullam nobis? Mollitia possimus voluptatum pariatur omnis quibusdam, quidem recusandae velit.</p>
         </f7-block>
-      </f7-tab>
-      <f7-tab id="tab3">
-        <f7-block>
+      </f7-page-content>
+      <f7-page-content id="tab3" tab>
+         <f7-block>
           <p>Tab 3</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure odio dolor, soluta reprehenderit, corporis officia earum corrupti amet ea aspernatur praesentium, distinctio ipsa atque officiis. Recusandae ab error, atque natus.</p>
           <p>Quibusdam repellendus, repudiandae nulla commodi ut nemo eum, quia dolorum dicta voluptate reprehenderit. Omnis, temporibus iure quia ad sit incidunt similique et quaerat dicta delectus aspernatur, unde illo cum ipsam?</p>
@@ -41,9 +41,9 @@
           <p>Autem ullam tempore suscipit consectetur, odit soluta. Neque quasi mollitia culpa autem, quis ab nostrum non distinctio illo magni perspiciatis et consequuntur possimus aliquam reprehenderit reiciendis praesentium consectetur, voluptas obcaecati?</p>
           <p>Quae repellendus aut iste provident consectetur inventore voluptatibus, saepe ad dolorem voluptatum recusandae praesentium animi harum officiis error nulla ullam nobis? Mollitia possimus voluptatum pariatur omnis quibusdam, quidem recusandae velit.</p>
         </f7-block>
-      </f7-tab>
-      <f7-tab id="tab4">
-        <f7-block>
+      </f7-page-content>
+      <f7-page-content id="tab4" tab>
+         <f7-block>
           <p>Tab 4</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure odio dolor, soluta reprehenderit, corporis officia earum corrupti amet ea aspernatur praesentium, distinctio ipsa atque officiis. Recusandae ab error, atque natus.</p>
           <p>Quibusdam repellendus, repudiandae nulla commodi ut nemo eum, quia dolorum dicta voluptate reprehenderit. Omnis, temporibus iure quia ad sit incidunt similique et quaerat dicta delectus aspernatur, unde illo cum ipsam?</p>
@@ -51,7 +51,7 @@
           <p>Autem ullam tempore suscipit consectetur, odit soluta. Neque quasi mollitia culpa autem, quis ab nostrum non distinctio illo magni perspiciatis et consequuntur possimus aliquam reprehenderit reiciendis praesentium consectetur, voluptas obcaecati?</p>
           <p>Quae repellendus aut iste provident consectetur inventore voluptatibus, saepe ad dolorem voluptatum recusandae praesentium animi harum officiis error nulla ullam nobis? Mollitia possimus voluptatum pariatur omnis quibusdam, quidem recusandae velit.</p>
         </f7-block>
-      </f7-tab>
+      </f7-page-content>
     </f7-tabs>
   </f7-page>
 </template>
