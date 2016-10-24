b8bc8276dab13f356ad4398f88d8b6066ba482e5
Vladimir Kharlampidi <nolimits4web@gmail.com>
2016-10-24 19:46:49 +0300

    Link props

--- src/components/list-button.vue
+++ src/components/list-button.vue
@@ -1,7 +1,30 @@
 <template>
   <li>
-    <a v-if="title" class="item-link list-button" :href="(typeof link !== 'string' ? '#' : link)" :class="{'external': external || linkExternal, 'back': back}" v-html="title"></a>
-    <a v-else class="item-link list-button" :href="(typeof link !== 'string' ? '#' : link)" :class="{'external': external || linkExternal, 'back': back}"><slot></slot></a>
+    <a v-if="title"
+      class="item-link list-button"
+      :href="(typeof link !== 'string' ? '#' : link)"
+      :class="classesObject"
+      :data-panel="typeof openPanel === 'string' ? openPanel : false"
+      :data-popup="typeof openPopover === 'string' ? openPopover : false"
+      :data-popover="typeof openPicker === 'string' ? openPicker : false"
+      :data-picker="typeof openLoginScreen === 'string' ? openLoginScreen : false"
+      :data-login-screen="typeof openSortable === 'string' ? openSortable : false"
+      :data-sortable="typeof toggleSortable === 'string' ? toggleSortable : false"
+      :data-tab="typeof tabLink === 'string' ? tabLink : false"
+      v-html="title"
+      ></a>
+    <a v-else
+      class="item-link list-button"
+      :href="(typeof link !== 'string' ? '#' : link)"
+      :class="classesObject"
+      :data-panel="typeof openPanel === 'string' ? openPanel : false"
+      :data-popup="typeof openPopover === 'string' ? openPopover : false"
+      :data-popover="typeof openPicker === 'string' ? openPicker : false"
+      :data-picker="typeof openLoginScreen === 'string' ? openLoginScreen : false"
+      :data-login-screen="typeof openSortable === 'string' ? openSortable : false"
+      :data-sortable="typeof toggleSortable === 'string' ? toggleSortable : false"
+      :data-tab="typeof tabLink === 'string' ? tabLink : false"
+      ><slot></slot></a>
   </li>
 </template>
 <script>
@@ -11,10 +34,77 @@
       'link': [String, Boolean],
       'external': Boolean,
       'link-external': Boolean,
-      'back': Boolean
+      'back': Boolean,
+
+      // View
+      view: String,
+
+      // Panel
+      openPanel: [String, Boolean],
+      closePanel: String,
+
+      // Popup
+      openPopup: [String, Boolean],
+      closePopup: Boolean,
+
+      // Popover
+      openPopover: [String, Boolean],
+      closePopover: Boolean,
+
+      // Login Screen
+      openLoginScreen: [String, Boolean],
+      closeLoginScreen: Boolean,
+
+      // Picker
+      openPicker: [String, Boolean],
+      closePicker: Boolean,
+
+      // Tab
+      tabLink: [Boolean, String],
+
+      // Sortable
+      openSortable: [String, Boolean],
+      closeSortable: Boolean,
+      toggleSortable: [String, Boolean],
     },
-    data: function () {
-      return {};
+    computed: {
+      classesObject: function () {
+        var self = this;
+        var co = {
+          'external': self.external || self.linkExternal,
+          'back': self.back
+        };
+
+        // Panel
+        if (self.closePanel) co['close-panel'] = true;
+        if (self.openPanel) co['open-panel'] = true;
+
+        // Popup
+        if (self.closePopup) co['close-popup'] = true;
+        if (self.openPopup) co['open-popup'] = true;
+
+        // Popover
+        if (self.closePopover) co['close-popover'] = true;
+        if (self.openPopover) co['open-popover'] = true;
+
+        // Picker
+        if (self.closePicker) co['close-picker'] = true;
+        if (self.openPicker) co['open-picker'] = true;
+
+        // Login Screen
+        if (self.closeLoginScreen) co['close-login-screen'] = true;
+        if (self.openLoginScreen) co['open-login-screen'] = true;
+
+        // Sortable
+        if (self.closeSortable) co['close-sortable'] = true;
+        if (self.openSortable) co['open-sortable'] = true;
+        if (self.toggleSortable) co['toggle-sortable'] = true;
+
+        // Tab
+        if (self.tabLink) co['tab-link'] = true;
+
+        return co;
+      }
     },
     methods: {
       onClick: function (event) {
