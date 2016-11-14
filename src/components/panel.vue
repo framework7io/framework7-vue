73d72bf5cc25ab316a878528dd3054248489786a
Vladimir Kharlampidi <nolimits4web@gmail.com>
2016-11-14 21:38:09 +0300

    Manage panel state
    Ref #4

--- src/components/panel.vue
+++ src/components/panel.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="panel" :class="classesObject">
+  <div
+    class="panel"
+    :class="classesObject"
+    :style="{'display': opened ? 'block' : ''}"
+    @open="onOpen"
+    @opened="onOpened"
+    @close="onClose"
+    @closed="onClosed"
+  >
     <slot></slot>
   </div>
 </template>
@@ -13,7 +21,8 @@
       'left': Boolean,
       'right': Boolean,
       'theme': String,
-      'layout': String
+      'layout': String,
+      'opened': Boolean
     },
     computed: {
       classesObject: function () {
@@ -25,7 +34,53 @@
         co['panel-' + effect] = true;
         if (self.layout) co['layout-' + self.layout] = true;
         if (self.theme) co['theme-' + self.theme] = true;
+        co['active'] = self.opened;
         return co;
+      }
+    },
+    watch: {
+      opened: function (opened) {
+        var self = this;
+        if (!self.$f7) return;
+        var side = self.side || (self.left ? 'left' : 'right');
+        if (opened) {
+          self.$f7.openPanel(side);
+        }
+        else {
+          self.$f7.closePanel(side);
+        }
+      }
+    },
+    mounted: function () {
+      var self = this;
+      var $$ = self.$$
+      if (!$$) return;
+      var side = self.side || (self.left ? 'left' : 'right');
+      var effect = self.effect || (self.reveal ? 'reveal' : 'cover');
+      if (self.opened) {
+        $$('body').addClass('with-panel-' + side + '-' + effect)
+      }
+    },
+    methods: {
+      onOpen: function (event) {
+        this.$emit('open', event);
+      },
+      onOpened: function (event) {
+        this.$emit('opened', event);
+      },
+      onClose: function (event) {
+        this.$emit('open', event);
+      },
+      onClosed: function (event) {
+        this.$emit('closed', event);
+      },
+      onF7Init: function () {
+        var self = this;
+        var $$ = self.$$
+        if (!$$) return;
+        if ($$(self.$root.$el).find('.panel-overlay').length === 0) {
+          $$('<div class="panel-overlay"></div>').insertBefore(self.$el)
+        }
       }
     }
   }
