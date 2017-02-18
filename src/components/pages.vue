2706ee36ab30138d860e4bccf9789de53f6ae1b0
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-02-18 15:44:08 +0300

    Delete page instead of adding empty object

--- src/components/pages.vue
+++ src/components/pages.vue
@@ -5,7 +5,7 @@
       var pages = [];
       for (var pageId in self.pages) {
         var page = self.pages[pageId];
-        pages.push(c(page.component, {tag: 'component', props: self.$route.params}))
+        pages.push(c(page.component, {tag: 'component', props: self.$route.params, key:pageId}))
       }
       return c('div',
         {
@@ -65,7 +65,7 @@
             break;
           }
         }
-        if (idToRemove) this.$set(this.pages, idToRemove, {});
+        if (idToRemove) this.$delete(this.pages, idToRemove);
       },
       onRouteChange: function (event) {
         var self = this;
