6529271a49029ed9b148d68b0f53d97beb467330
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-03-16 14:31:49 +0300

    theme support

--- src/components/statusbar.vue
+++ src/components/statusbar.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="statusbar-overlay"></div>
+  <div class="statusbar-overlay" :class="(theme ? 'theme-' + theme : undefined)"></div>
 </template>
 <script>
-  export default {}
+  export default {
+    props: {
+      theme: String
+    }
+  }
 </script>
