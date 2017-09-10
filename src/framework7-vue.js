1721d3fda429c7602e35c4bd7522e583f25cc1f4
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-11 00:03:52 +0300

    entry

---
+++ src/framework7-vue.js
@@ -0,0 +1,5 @@
+import VuePlugin from './utils/vue-plugin';
+import f7View from './components/view.vue';
+import f7Page from './components/page.vue';
+
+export { VuePlugin as Framework7Vue, f7View, f7Page };
