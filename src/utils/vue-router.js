75ef9dcc4364afbd8f248dd16c20b935c7394d1f
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-12 16:00:18 +0300

    Renames

--- src/utils/vue-router.js
+++ src/utils/vue-router.js
@@ -6,8 +6,8 @@ export default {
     pageComponentLoader(routerEl, component, componentUrl, options, resolve, reject) {
       const router = this;
       const el = router.$el[0];
-      const vueRouter = el.__vue__;
-      if (!vueRouter || !vueRouter.pages) {
+      const routerVue = el.__vue__;
+      if (!routerVue || !routerVue.pages) {
         reject();
       }
       const id = Utils.now();
@@ -17,9 +17,9 @@ export default {
         params: Utils.extend({}, options.route.params),
         route: Utils.extend({}, options.route),
       };
-      vueRouter.$route = options.route;
-      vueRouter.pages.push(pageData);
-      vueRouter.$nextTick(() => {
+      routerVue.$route = options.route;
+      routerVue.pages.push(pageData);
+      routerVue.$nextTick(() => {
         const pageEl = el.childNodes[el.childNodes.length - 1];
         pageData.el = pageEl;
         resolve(pageEl);
@@ -28,7 +28,7 @@ export default {
     removePage($pageEl) {
       if (!$pageEl) return;
       const router = this;
-      const vueRouter = router.$el[0].__vue__;
+      const routerVue = router.$el[0].__vue__;
 
       let pageEl;
       if ('length' in $pageEl) {
@@ -41,9 +41,9 @@ export default {
 
       if ($pageEl.length === 0) return;
 
-      vueRouter.pages.forEach((page, index) => {
+      routerVue.pages.forEach((page, index) => {
         if (page.el === pageEl) {
-          vueRouter.pages.splice(index, 1);
+          routerVue.pages.splice(index, 1);
         }
       });
     },
