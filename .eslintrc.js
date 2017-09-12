ac5504c0ad93a9307f6091d056e5929082d4d91e
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-12 16:02:01 +0300

    Add HTML plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,28 @@
 module.exports = {
-  'extends': 'airbnb-base',
-  'plugins': [
-    "import"
+  "extends": "airbnb-base",
+  "plugins": [
+    "import",
+    "html",
   ],
-  'globals': {
+  "globals": {
     "window": true,
     "document": true
   },
   rules: {
-    'max-len': ['error', 1000, 2, {
+    "prefer-destructuring": ["off"],
+    "max-len": ["error", 1000, 2, {
       ignoreUrls: true,
       ignoreComments: false,
       ignoreRegExpLiterals: true,
       ignoreStrings: true,
       ignoreTemplateLiterals: true,
     }],
-    'comma-dangle': ['error', {
-      arrays: 'always-multiline',
-      objects: 'always-multiline',
-      imports: 'always-multiline',
-      exports: 'always-multiline',
-      functions: 'never',
+    "comma-dangle": ["error", {
+      arrays: "always-multiline",
+      objects: "always-multiline",
+      imports: "always-multiline",
+      exports: "always-multiline",
+      functions: "never",
     }],
   },
 };
