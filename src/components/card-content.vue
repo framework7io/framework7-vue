01df9d7bd479318118c3193cf5ad93c0a1ce8100
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-12 15:59:01 +0300

    Card component

---
+++ src/components/card-content.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card-content" :class="{'card-content-padding': padding}">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      padding: {
+        type: Boolean,
+        default: true,
+      },
+    },
+  };
+</script>
---
+++ src/components/card.vue
@@ -0,0 +1,39 @@
+<script>
+  import f7CardHeader from './card-header.vue';
+  import f7CardContent from './card-content.vue';
+  import f7CardFooter from './card-footer.vue';
+
+  export default {
+    components: {
+      f7CardHeader,
+      f7CardContent,
+      f7CardFooter,
+    },
+    render(c) {
+      const self = this;
+      let headerEl;
+      let contentEl;
+      let footerEl;
+
+      if (self.title || (self.$slots && self.$slots.header)) {
+        headerEl = c('f7-card-header', [self.title, self.$slots.header]);
+      }
+      if (self.content || (self.$slots && self.$slots.content)) {
+        contentEl = c('f7-card-content', { props: { padding: self.padding } }, [self.content, self.$slots.content]);
+      }
+      if (self.footer || (self.$slots && self.$slots.footer)) {
+        footerEl = c('f7-card-footer', [self.footer, self.$slots.footer]);
+      }
+      return c('div', { staticClass: 'card' }, [headerEl, contentEl, footerEl, self.$slots.default]);
+    },
+    props: {
+      title: [String, Number],
+      content: [String, Number],
+      footer: [String, Number],
+      padding: {
+        type: Boolean,
+        default: true,
+      },
+    },
+  };
+</script>
