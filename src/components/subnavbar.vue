fe12131eb1de2ad7cc998c7c57e196aa667ab955
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-12 16:55:40 +0300

    Subnavbar component

---
+++ src/components/subnavbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="subnavbar" :class="sliding ? 'sliding' : ''">
+    <div class="subnavbar-inner" v-if="inner">
+      <slot></slot>
+    </div>
+    <slot v-else></slot>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      sliding: Boolean,
+      inner: {
+        type: Boolean,
+        default: true,
+      },
+    },
+  };
+</script>
