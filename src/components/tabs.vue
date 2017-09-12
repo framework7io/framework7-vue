6bb96ae633db1e6591da18211d3506f806929d17
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-12 19:51:16 +0300

    Tabs component

---
+++ src/components/tabs.vue
@@ -0,0 +1,22 @@
+<script>
+  export default {
+    render(c) {
+      const self = this;
+      const tabsEl = c('div', { staticClass: 'tabs' }, [self.$slots.default]);
+      if (self.animated || self.swipeable) return c('div', { class: self.classes }, [tabsEl]);
+      return tabsEl;
+    },
+    props: {
+      animated: Boolean,
+      swipeable: Boolean,
+    },
+    computed: {
+      classes() {
+        return {
+          'tabs-animated-wrap': this.animated,
+          'tabs-swipeable-wrap': this.swipeable,
+        };
+      },
+    },
+  };
+</script>
