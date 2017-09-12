fefba210df9bf00d928c4000bd8bb305e551e5d0
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-12 16:01:40 +0300

    Updated KS build

--- scripts/build-ks.js
+++ scripts/build-ks.js
@@ -1,6 +1,6 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 /* eslint no-console: "off" */
-
+const path = require('path');
 const gulp = require('gulp');
 const rollup = require('rollup-stream');
 const buble = require('rollup-plugin-buble');
@@ -8,29 +8,27 @@ const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const replace = require('rollup-plugin-replace');
 const resolve = require('rollup-plugin-node-resolve');
-const vue = require('rollup-plugin-vue2');
-
-const banner = require('./banner.js');
+const vue = require('rollup-plugin-vue');
+const sourcemaps = require('gulp-sourcemaps');
 
 function build(cb) {
   const env = process.env.NODE_ENV || 'development';
   const target = process.env.TARGET || 'universal';
   rollup({
-    entry: './kitchen-sink/app.js',
+    input: './kitchen-sink/app.js',
     plugins: [
       vue(),
       replace({
         'process.env.NODE_ENV': JSON.stringify(env), // or 'production'
         'process.env.TARGET': JSON.stringify(target),
+        'framework7-vue': () => path.resolve(__dirname, '../src/framework7-vue'),
       }),
       resolve({ jsnext: true }),
       buble(),
     ],
-    format: 'umd',
-    moduleName: 'KS',
-    useStrict: true,
-    sourceMap: true,
-    banner,
+    format: 'iife',
+    strict: true,
+    sourcemap: true,
   })
     .on('error', (err) => {
       if (cb) cb();
@@ -38,6 +36,8 @@ function build(cb) {
     })
     .pipe(source('app.js', './kitchen-sink'))
     .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./kitchen-sink/js/'))
     .on('end', () => {
       if (cb) cb();
