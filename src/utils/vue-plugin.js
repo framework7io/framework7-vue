8273d1480a63f85dcd2e72c311c83167a25a588a
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-12 16:30:11 +0300

    Default {} for params

--- src/utils/vue-plugin.js
+++ src/utils/vue-plugin.js
@@ -20,8 +20,8 @@ export default {
     Vue.prototype.$theme = $theme;
 
     // Init F7
-    function initFramework7(rootEl, params = {}, routes) {
-      const f7Params = Utils.extend({}, params, { root: rootEl });
+    function initFramework7(rootEl, params, routes) {
+      const f7Params = Utils.extend({}, (params || {}), { root: rootEl });
       if (routes && routes.length && !f7Params.routes) f7Params.routes = routes;
 
       f7Instance = new Framework7(f7Params);
