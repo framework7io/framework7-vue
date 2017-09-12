35cd861f3810c3d5432ee19a97c9c22c58295163
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-12 16:40:48 +0300

    Grid components

---
+++ src/components/col.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="('col-' + width) + (tabletWidth ? ' tablet-' + tabletWidth : '') + (desktopWidth ? ' desktop-' + desktopWidth : '')"><slot></slot></div>
+</template>
+<script>
+  export default {
+    props: {
+      width: {
+        type: [Number, String],
+        default: 'auto',
+      },
+      tabletWidth: {
+        type: [Number, String],
+      },
+      desktopWidth: {
+        type: [Number, String],
+      },
+    },
+    data() {
+      return {};
+    },
+  };
+</script>
---
+++ src/components/row.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="row" :class="{'no-gap': noGap}"><slot></slot></div>
+</template>
+<script>
+  export default {
+    props: {
+      noGap: Boolean,
+    },
+  };
+</script>
