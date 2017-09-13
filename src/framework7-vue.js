e6ce2e9632b7349dcc9400229c67ffb63c081dd5
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-13 18:22:09 +0300

    Import more components

--- src/framework7-vue.js
+++ src/framework7-vue.js
@@ -1,5 +1,56 @@
 import VuePlugin from './utils/vue-plugin';
+import f7Views from './components/views.vue';
 import f7View from './components/view.vue';
 import f7Page from './components/page.vue';
+import f7Navbar from './components/navbar.vue';
+import f7NavLeft from './components/nav-left.vue';
+import f7NavRight from './components/nav-right.vue';
+import f7NavTitle from './components/nav-title.vue';
+import f7Block from './components/block.vue';
+import f7BlockTitle from './components/block-title.vue';
+import f7BlockHeader from './components/block-header.vue';
+import f7BlockFooter from './components/block-footer.vue';
+import f7Card from './components/card.vue';
+import f7CardHeader from './components/card-header.vue';
+import f7CardFooter from './components/card-footer.vue';
+import f7CardContent from './components/card-content.vue';
+import f7Chip from './components/chip.vue';
+import f7Icon from './components/icon.vue';
+import f7Col from './components/col.vue';
+import f7Row from './components/row.vue';
+import f7Badge from './components/badge.vue';
+import f7Preloader from './components/preloader.vue';
+import f7Statusbar from './components/statusbar.vue';
+import f7Subnavbar from './components/subnavbar.vue';
+import f7Tabs from './components/tabs.vue';
+import f7Tab from './components/tab.vue';
 
-export { VuePlugin as Framework7Vue, f7View, f7Page };
+export {
+  VuePlugin as Framework7Vue,
+  f7Views,
+  f7View,
+  f7Page,
+  f7Navbar,
+  f7NavLeft,
+  f7NavRight,
+  f7NavTitle,
+  f7Block,
+  f7BlockTitle,
+  f7BlockHeader,
+  f7BlockFooter,
+  f7Card,
+  f7CardHeader,
+  f7CardFooter,
+  f7CardContent,
+  f7Chip,
+  f7Icon,
+  f7Col,
+  f7Row,
+  f7Badge,
+  f7Preloader,
+  f7Statusbar,
+  f7Subnavbar,
+  f7Tabs,
+  f7Tab,
+};
+
