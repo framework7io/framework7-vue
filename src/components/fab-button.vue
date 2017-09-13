c2b614a0cc35e7ea01053866381628d1a9448ead
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-13 22:21:14 +0300

    FAB Component

---
+++ src/components/fab-button.vue
@@ -0,0 +1,15 @@
+<template>
+  <a :class="color ? `color-${color}` : false" @click="onClick"><slot></slot></a>
+</template>
+<script>
+  export default {
+    props: {
+      color: String,
+    },
+    methods: {
+      onClick(event) {
+        this.$emit('click', event);
+      },
+    },
+  };
+</script>
---
+++ src/components/fab-buttons.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="fab-buttons" :class="classes"><slot></slot></div>
+</template>
+<script>
+  export default {
+    props: {
+      color: String,
+      position: {
+        type: String,
+        default: 'top',
+      },
+    },
+    computed: {
+      classes() {
+        const self = this;
+        return {
+          [`color-${self.color}`]: self.color,
+          [`fab-buttons-${self.position}`]: true,
+        };
+      },
+    },
+  };
+</script>
