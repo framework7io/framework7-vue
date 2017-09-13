0f41f653693219ffc6e04fb50cba2fc59b47b4a6
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-13 18:21:21 +0300

    Views component

---
+++ src/components/views.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="views" :class="classes"><slot></slot></div>
+</template>
+<script>
+  export default {
+    props: {
+      tabs: Boolean,
+    },
+    computed: {
+      classes() {
+        const co = {
+          tabs: this.tabs,
+        };
+        return co;
+      },
+    },
+  };
+</script>
