d015fcb503672e256df04e6ccff0b3ec450d2897
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-13 21:43:43 +0300

    More configurable Page Content Component

--- src/components/page-content.vue
+++ src/components/page-content.vue
@@ -3,9 +3,61 @@
 </template>
 <script>
   export default {
+    render(c) {
+      const self = this;
+
+      let ptrEl;
+      let infiniteEl;
+
+      if (self.ptr && (self.ptrPreloader)) {
+        ptrEl = c('div', { staticClass: 'ptr-preloader' }, [
+          c('div', { staticClass: 'preloader' }),
+          c('div', { staticClass: 'ptr-arrow' }),
+        ]);
+      }
+      if ((self.infinite) && self.infinitePreloader) {
+        infiniteEl = c('div', { staticClass: 'preloader infinite-scroll-preloader' });
+      }
+      return c('div', {
+        staticClass: 'page-content',
+        class: self.classes,
+        attrs: {
+          'data-ptr-distance': self.ptrDistance,
+          'data-infinite-distance': self.infiniteDistance,
+        },
+        on: {
+          'ptr:pullstart': self.onPtrPullStart,
+          'ptr:pullmove': self.onPtrPullMove,
+          'ptr:pullend': self.onPtrPullEnd,
+          'ptr:refresh': self.onPtrRefresh,
+          'ptr:done': self.onPtrRefreshDone,
+          infinite: self.onInfinite,
+          'tab:show': self.onTabShow,
+          'tab:hide': self.onTabHide,
+        },
+      }, (self.infiniteTop ? [ptrEl, infiniteEl, self.$slots.default] : [ptrEl, self.$slots.default, infiniteEl]));
+    },
     props: {
       tab: Boolean,
       tabActive: Boolean,
+      ptr: Boolean,
+      ptrDistance: Number,
+      ptrPreloader: {
+        type: Boolean,
+        default: true,
+      },
+      infinite: Boolean,
+      infiniteTop: Boolean,
+      infiniteDistance: Number,
+      infinitePreloader: {
+        type: Boolean,
+        default: true,
+      },
+      hideBarsOnScroll: Boolean,
+      hideNavbarOnScroll: Boolean,
+      hideToolbarOnScroll: Boolean,
+      messagesContent: Boolean,
+      loginScreen: Boolean,
     },
     computed: {
       classes() {
@@ -13,10 +65,36 @@
         return {
           tab: self.tab,
           'tab-active': self.tabActive,
+          'ptr-content': this.ptr,
+          'infinite-scroll-content': this.infinite,
+          'infinite-scroll-top': this.infiniteTop,
+          'hide-bars-on-scroll': this.hideBarsOnScroll,
+          'hide-navbar-on-scroll': this.hideNavbarOnScroll,
+          'hide-toolbar-on-scroll': this.hideToolbarOnScroll,
+          'messages-content': this.messagesContent,
+          'login-screen-content': this.loginScreen,
         };
       },
     },
     methods: {
+      onPtrPullStart(event) {
+        this.$emit('ptr:pullstart', event);
+      },
+      onPtrPullMove(event) {
+        this.$emit('ptr:pullmove', event);
+      },
+      onPtrPullEnd(event) {
+        this.$emit('ptr:pullend', event);
+      },
+      onPtrRefresh(event) {
+        this.$emit('ptr:refresh', event.detail);
+      },
+      onPtrRefreshDone(event) {
+        this.$emit('ptr:done', event);
+      },
+      onInfinite(event) {
+        this.$emit('infinite', event);
+      },
       onTabShow(e) {
         const self = this;
         self.$emit('tab:show', e);
