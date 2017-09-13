77072215589cb1e3cee6ba2ca9da3ed1f2f5064b
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-13 21:16:59 +0300

    Page Content Component

---
+++ src/components/page-content.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="page-content" :class="classes" @tab:show="onTabShow" @tab:hide="onTabHide"><slot></slot></div>
+</template>
+<script>
+  export default {
+    props: {
+      tab: Boolean,
+      tabActive: Boolean,
+    },
+    computed: {
+      classes() {
+        const self = this;
+        return {
+          tab: self.tab,
+          'tab-active': self.tabActive,
+        };
+      },
+    },
+    methods: {
+      onTabShow(e) {
+        const self = this;
+        self.$emit('tab:show', e);
+      },
+      onTabHide(e) {
+        const self = this;
+        self.$emit('tab:hide', e);
+      },
+    },
+  };
+</script>
