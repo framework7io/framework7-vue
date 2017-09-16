66b775af8a47606e38d19bd3bc46f55758f1f57d
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-16 22:21:33 +0300

    Swipeout component

---
+++ src/components/swipeout-actions.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="`swipeout-actions-${sideComputed}`">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      left: Boolean,
+      right: Boolean,
+      side: String,
+    },
+    computed: {
+      sideComputed() {
+        if (!this.side) {
+          if (this.left) return 'left';
+          if (this.right) return 'right';
+          return 'right';
+        }
+        return this.side;
+      },
+    },
+    data() {
+      return {};
+    },
+  };
+</script>
---
+++ src/components/swipeout-button.vue
@@ -0,0 +1,30 @@
+<template>
+  <a @click="onClick" :class="classes"><slot>{{text}}</slot></a>
+</template>
+<script>
+  export default {
+    props: {
+      text: String,
+      overswipe: Boolean,
+      close: Boolean,
+      delete: Boolean,
+      color: String,
+    },
+    computed: {
+      classes() {
+        const co = {
+          'swipeout-overswipe': this.overswipe,
+          'swipeout-delete': this.delete,
+          'swipeout-close': this.close,
+          [`color-${this.color}`]: this.color,
+        };
+        return co;
+      },
+    },
+    methods: {
+      onClick(event) {
+        this.$emit('click', event);
+      },
+    },
+  };
+</script>
