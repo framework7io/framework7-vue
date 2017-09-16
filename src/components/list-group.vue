7f0195e0a1e47d34f16ee11dc2f2418fb0fd9587
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-16 21:31:19 +0300

    List Group component

---
+++ src/components/list-group.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="list-group">
+    <ul>
+      <slot></slot>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      mediaList: Boolean,
+      sortable: Boolean,
+    },
+    computed: {
+      sortableComputed() {
+        return this.sortable || this.$parent.sortable;
+      },
+      mediaListComputed() {
+        return this.mediaList || this.$parent.mediaList;
+      },
+    },
+    data() {
+      return {};
+    },
+  };
+</script>
