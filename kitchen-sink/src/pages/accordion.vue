5cea178f3386cb3ede9c0751794836c081748a3d
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-18 18:21:04 +0300

    Accordion demo

--- kitchen-sink/src/pages/accordion.vue
+++ kitchen-sink/src/pages/accordion.vue
@@ -1,227 +1,95 @@
 <template>
-  <div class="page">
+  <f7-page>
     <f7-navbar title="Accordion" back-link="Back"></f7-navbar>
-    <div class="page-content">
-      <f7-block-title>List View Accordion</f7-block-title>
-      <div class="list accordion-list">
-        <ul>
-          <li class="accordion-item">
-            <a href="#" class="item-link item-content">
-              <div class="item-inner">
-                <div class="item-title">Lorem Ipsum</div>
-              </div>
-            </a>
-            <div class="accordion-item-content">
-              <div class="block">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elementum id neque nec commodo. Sed vel justo at turpis laoreet pellentesque quis sed lorem. Integer semper arcu nibh, non mollis arcu tempor vel. Sed pharetra tortor vitae est rhoncus, vel congue dui sollicitudin. Donec eu arcu dignissim felis viverra blandit suscipit eget ipsum.
-                </p>
-              </div>
-            </div>
-          </li>
-          <li class="accordion-item">
-            <a href="#" class="item-link item-content">
-              <div class="item-inner">
-                <div class="item-title">Nested List</div>
-              </div>
-            </a>
-            <div class="accordion-item-content">
-              <div class="list">
-                <ul>
-                  <li>
-                    <div class="item-content">
-                      <div class="item-media"> <i class="icon icon-f7"></i>
-                      </div>
-                      <div class="item-inner">
-                        <div class="item-title">Item 1</div>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="item-content">
-                      <div class="item-media"> <i class="icon icon-f7"></i>
-                      </div>
-                      <div class="item-inner">
-                        <div class="item-title">Item 2</div>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="item-content">
-                      <div class="item-media">
-                        <i class="icon icon-f7"></i>
-                      </div>
-                      <div class="item-inner">
-                        <div class="item-title">Item 3</div>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="item-content">
-                      <div class="item-media">
-                        <i class="icon icon-f7"></i>
-                      </div>
-                      <div class="item-inner">
-                        <div class="item-title">Item 4</div>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </li>
-          <li class="accordion-item">
-            <a href="#" class="item-link item-content">
-              <div class="item-inner">
-                <div class="item-title">Integer semper</div>
-              </div>
-            </a>
-            <div class="accordion-item-content">
-              <div class="block">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elementum id neque nec commodo. Sed vel justo at turpis laoreet pellentesque quis sed lorem. Integer semper arcu nibh, non mollis arcu tempor vel. Sed pharetra tortor vitae est rhoncus, vel congue dui sollicitudin. Donec eu arcu dignissim felis viverra blandit suscipit eget ipsum.
-                </p>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <f7-block-title>Inset Accordion</f7-block-title>
-      <div class="list accordion-list inset">
-        <ul>
-          <li class="accordion-item">
-            <a href="#" class="item-link item-content">
-              <div class="item-inner">
-                <div class="item-title">Lorem Ipsum</div>
-              </div>
-            </a>
-            <div class="accordion-item-content">
-              <div class="block">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elementum id neque nec commodo. Sed vel justo at turpis laoreet pellentesque quis sed lorem. Integer semper arcu nibh, non mollis arcu tempor vel. Sed pharetra tortor vitae est rhoncus, vel congue dui sollicitudin. Donec eu arcu dignissim felis viverra blandit suscipit eget ipsum.
-                </p>
-              </div>
-            </div>
-          </li>
-          <li class="accordion-item">
-            <a href="#" class="item-link item-content">
-              <div class="item-inner">
-                <div class="item-title">Nested List</div>
-              </div>
-            </a>
-            <div class="accordion-item-content">
-              <div class="list">
-                <ul>
-                  <li>
-                    <div class="item-content">
-                      <div class="item-media">
-                        <i class="icon icon-f7"></i>
-                      </div>
-                      <div class="item-inner">
-                        <div class="item-title">Item 1</div>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="item-content">
-                      <div class="item-media">
-                        <i class="icon icon-f7"></i>
-                      </div>
-                      <div class="item-inner">
-                        <div class="item-title">Item 2</div>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="item-content">
-                      <div class="item-media">
-                        <i class="icon icon-f7"></i>
-                      </div>
-                      <div class="item-inner">
-                        <div class="item-title">Item 3</div>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="item-content">
-                      <div class="item-media">
-                        <i class="icon icon-f7"></i>
-                      </div>
-                      <div class="item-inner">
-                        <div class="item-title">Item 4</div>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </li>
-          <li class="accordion-item">
-            <a href="#" class="item-link item-content">
-              <div class="item-inner">
-                <div class="item-title">Integer semper</div>
-              </div>
-            </a>
-            <div class="accordion-item-content">
-              <div class="block">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elementum id neque nec commodo. Sed vel justo at turpis laoreet pellentesque quis sed lorem. Integer semper arcu nibh, non mollis arcu tempor vel. Sed pharetra tortor vitae est rhoncus, vel congue dui sollicitudin. Donec eu arcu dignissim felis viverra blandit suscipit eget ipsum.
-                </p>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <f7-block-title>Custom Styled Accordion</f7-block-title>
-      <div class="block accordion-list custom-accordion">
-        <div class="accordion-item">
-          <div class="accordion-item-toggle">
-            <i class="icon icon-ks-plus">+</i>
-            <i class="icon icon-ks-minus">-</i>
-            <span>Item 1</span>
-          </div>
-          <div class="accordion-item-content">
+
+    <f7-block-title>List View Accordion</f7-block-title>
+    <f7-list accordion-list>
+      <f7-list-item accordion-item title="Lorem Ipsum">
+        <f7-accordion-content>
+          <f7-block>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elementum id neque nec commodo. Sed vel justo at turpis laoreet pellentesque quis sed lorem. Integer semper arcu nibh, non mollis arcu tempor vel. Sed pharetra tortor vitae est rhoncus, vel congue dui sollicitudin. Donec eu arcu dignissim felis viverra blandit suscipit eget ipsum.
             </p>
-          </div>
-        </div>
-        <div class="accordion-item">
-          <div class="accordion-item-toggle">
-            <i class="icon icon-ks-plus">+</i>
-            <i class="icon icon-ks-minus">-</i>
-            <span>Item 2</span>
-          </div>
-          <div class="accordion-item-content">
+          </f7-block>
+        </f7-accordion-content>
+      </f7-list-item>
+      <f7-list-item accordion-item title="Nested List">
+        <f7-accordion-content>
+          <f7-list>
+            <f7-list-item title="Item 1"></f7-list-item>
+            <f7-list-item title="Item 2"></f7-list-item>
+            <f7-list-item title="Item 3"></f7-list-item>
+            <f7-list-item title="Item 4"></f7-list-item>
+          </f7-list>
+        </f7-accordion-content>
+      </f7-list-item>
+      <f7-list-item accordion-item title="Integer semper">
+        <f7-accordion-content>
+          <f7-block>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elementum id neque nec commodo. Sed vel justo at turpis laoreet pellentesque quis sed lorem. Integer semper arcu nibh, non mollis arcu tempor vel. Sed pharetra tortor vitae est rhoncus, vel congue dui sollicitudin. Donec eu arcu dignissim felis viverra blandit suscipit eget ipsum.
             </p>
-          </div>
-        </div>
-        <div class="accordion-item">
-          <div class="accordion-item-toggle">
-            <i class="icon icon-ks-plus">+</i>
-            <i class="icon icon-ks-minus">-</i>
-            <span>Item 3</span>
-          </div>
-          <div class="accordion-item-content">
+          </f7-block>
+        </f7-accordion-content>
+      </f7-list-item>
+    </f7-list>
+
+    <f7-block-title>Inset Accordion</f7-block-title>
+    <f7-list accordion-list inset>
+      <f7-list-item accordion-item title="Lorem Ipsum">
+        <f7-accordion-content>
+          <f7-block>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elementum id neque nec commodo. Sed vel justo at turpis laoreet pellentesque quis sed lorem. Integer semper arcu nibh, non mollis arcu tempor vel. Sed pharetra tortor vitae est rhoncus, vel congue dui sollicitudin. Donec eu arcu dignissim felis viverra blandit suscipit eget ipsum.
             </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+          </f7-block>
+        </f7-accordion-content>
+      </f7-list-item>
+      <f7-list-item accordion-item title="Nested List">
+        <f7-accordion-content>
+          <f7-list>
+            <f7-list-item title="Item 1"></f7-list-item>
+            <f7-list-item title="Item 2"></f7-list-item>
+            <f7-list-item title="Item 3"></f7-list-item>
+            <f7-list-item title="Item 4"></f7-list-item>
+          </f7-list>
+        </f7-accordion-content>
+      </f7-list-item>
+      <f7-list-item accordion-item title="Integer semper">
+        <f7-accordion-content>
+          <f7-block>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elementum id neque nec commodo. Sed vel justo at turpis laoreet pellentesque quis sed lorem. Integer semper arcu nibh, non mollis arcu tempor vel. Sed pharetra tortor vitae est rhoncus, vel congue dui sollicitudin. Donec eu arcu dignissim felis viverra blandit suscipit eget ipsum.
+            </p>
+          </f7-block>
+        </f7-accordion-content>
+      </f7-list-item>
+    </f7-list>
+
+    <f7-block-title>Custom Collapsible</f7-block-title>
+    <f7-block inner accordion-list>
+      <f7-accordion-item v-for="n in 3" :key="n">
+        <f7-accordion-toggle><b>Item {{n}}</b></f7-accordion-toggle>
+        <f7-accordion-content>Content {{n}}</f7-accordion-content>
+      </f7-accordion-item>
+    </f7-block>
+
+  </f7-page>
 </template>
 <script>
-  import { f7Navbar, f7Page, f7BlockTitle } from 'framework7-vue';
+  import { f7Navbar, f7Page, f7BlockTitle, f7Block, f7AccordionItem, f7AccordionToggle, f7AccordionContent, f7List, f7ListItem } from 'framework7-vue';
 
   export default {
     components: {
       f7Navbar,
       f7Page,
       f7BlockTitle,
+      f7Block,
+      f7AccordionItem,
+      f7AccordionToggle,
+      f7AccordionContent,
+      f7List,
+      f7ListItem,
     }
   }
 </script>
