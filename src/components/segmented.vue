daa3bf31a2900db7e17a630ba04039d00362fd2d
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-18 16:26:44 +0300

    Segmented component

---
+++ src/components/segmented.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="segmented" :class="(color ? 'color-' + color : '')"><slot></slot></div>
+</template>
+<script>
+  export default {
+    props: {
+      color: String,
+      raised: Boolean,
+      round: Boolean,
+    },
+    computed: {
+      classes() {
+        const self = this;
+        return {
+          [`color-${self.color}`]: self.color,
+          'segmented-raised': self.raised,
+          'segmented-round': self.round,
+        };
+      },
+    },
+  };
+</script>
