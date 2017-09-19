78657b46e553121e445203af0e7b9b80b06ed9b8
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-19 15:14:51 +0300

    Toggle demo

--- kitchen-sink/src/pages/toggle.vue
+++ kitchen-sink/src/pages/toggle.vue
@@ -1,79 +1,54 @@
 <template>
-<div class="page">
-  <f7-navbar title="Toggle" back-link="Back"></f7-navbar>
-  <div class="page-content">
+  <f7-page>
+    <f7-navbar title="Toggle" back-link="Back"></f7-navbar>
     <f7-block-title>Super Heroes</f7-block-title>
-    <div class="list simple-list">
-      <ul>
-        <li>
-          <span>Batman</span>
-          <label class="toggle">
-            <input type="checkbox" checked>
-            <span class="toggle-icon"></span>
-          </label>
-        </li>
-        <li>
-          <span>Aquaman</span>
-          <label class="toggle color-cyan">
-            <input type="checkbox" checked>
-            <span class="toggle-icon"></span>
-          </label>
-        </li>
-        <li>
-          <span>Superman</span>
-          <label class="toggle color-red">
-            <input type="checkbox" checked>
-            <span class="toggle-icon"></span>
-          </label>
-        </li>
-        <li>
-          <span>Hulk</span>
-          <label class="toggle color-lightgreen">
-            <input type="checkbox">
-            <span class="toggle-icon"></span>
-          </label>
-        </li>
-        <li>
-          <span>Spiderman (Disabled)</span>
-          <label class="toggle disabled">
-            <input type="checkbox">
-            <span class="toggle-icon"></span>
-          </label>
-        </li>
-        <li>
-          <span>Ironman (Disabled)</span>
-          <label class="toggle">
-            <input type="checkbox" checked disabled>
-            <span class="toggle-icon"></span>
-          </label>
-        </li>
-        <li>
-          <span>Thor</span>
-          <label class="toggle color-orange">
-            <input type="checkbox" checked>
-            <span class="toggle-icon"></span>
-          </label>
-        </li>
-        <li>
-          <span>Wonder Woman</span>
-          <label class="toggle color-pink">
-            <input type="checkbox">
-            <span class="toggle-icon"></span>
-          </label>
-        </li>
-      </ul>
-    </div>
-  </div>
-</div>
+    <f7-list simple-list>
+      <f7-list-item>
+        <span>Batman</span>
+        <f7-toggle checked></f7-toggle>
+      </f7-list-item>
+      <f7-list-item>
+        <span>Aquaman</span>
+        <f7-toggle checked color="cyan"></f7-toggle>
+      </f7-list-item>
+      <f7-list-item>
+        <span>Superman</span>
+        <f7-toggle checked color="red"></f7-toggle>
+      </f7-list-item>
+      <f7-list-item>
+        <span>Hulk</span>
+        <f7-toggle color="light-green"</f7-toggle>
+      </f7-list-item>
+      <f7-list-item>
+        <span>Spiderman (Disabled)</span>
+        <f7-toggle disabled></f7-toggle>
+      </f7-list-item>
+      <f7-list-item>
+        <span>Ironman (Disabled)</span>
+        <f7-toggle checked disabled></f7-toggle>
+      </f7-list-item>
+      <f7-list-item>
+        <span>Thor</span>
+        <f7-toggle checked color="orange"></f7-toggle>
+      </f7-list-item>
+      <f7-list-item>
+        <span>Wonder Woman</span>
+        <f7-toggle color="pink"></f7-toggle>
+      </f7-list-item>
+    </f7-list>
+  </f7-page>
 </template>
 <script>
-  import { f7Navbar, f7Page, f7BlockTitle } from 'framework7-vue';
+  import { f7Navbar, f7Page, f7BlockTitle, f7List, f7ListItem, f7Toggle } from 'framework7-vue';
 
   export default {
     components: {
       f7Navbar,
       f7Page,
       f7BlockTitle,
-    }
-  }
+      f7List,
+      f7ListItem,
+      f7Toggle,
+    },
+  };
 </script>
