cb4966a07517082e4c385c06fdebebe8060dbe8d
Pooya Parsa <pooya@pi0.ir>
2017-09-21 00:03:54 +0430

    remove extra f7-list

--- kitchen-sink/src/pages/radio.vue
+++ kitchen-sink/src/pages/radio.vue
@@ -17,46 +17,45 @@
 
 
     <f7-block-title>With Media Lists</f7-block-title>
-    <f7-list>
       <f7-list media-list>
-      <f7-list-item
-        radio
-        checked
-        name="demo-media-radio"
-        value="1"
-        title="Facebook"
-        after="17:14"
-        subtitle="New messages from John Doe"
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis tellus ut turpis condimentum, ut dignissim lacus tincidunt. Cras dolor metus, ultrices condimentum sodales sit amet, pharetra sodales eros. Phasellus vel felis tellus. Mauris rutrum ligula nec dapibus feugiat. In vel dui laoreet, commodo augue id, pulvinar lacus."
-      ></f7-list-item>
-      <f7-list-item
-        radio
-        name="demo-media-radio"
-        value="2"
-        title="John Doe (via Twitter)"
-        after="17:11"
-        subtitle="John Doe (@_johndoe) mentioned you on Twitter!"
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis tellus ut turpis condimentum, ut dignissim lacus tincidunt. Cras dolor metus, ultrices condimentum sodales sit amet, pharetra sodales eros. Phasellus vel felis tellus. Mauris rutrum ligula nec dapibus feugiat. In vel dui laoreet, commodo augue id, pulvinar lacus."
-      ></f7-list-item>
-      <f7-list-item
-        radio
-        name="demo-media-radio"
-        value="3"
-        title="Facebook"
-        after="16:48"
-        subtitle="New messages from John Doe"
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis tellus ut turpis condimentum, ut dignissim lacus tincidunt. Cras dolor metus, ultrices condimentum sodales sit amet, pharetra sodales eros. Phasellus vel felis tellus. Mauris rutrum ligula nec dapibus feugiat. In vel dui laoreet, commodo augue id, pulvinar lacus."
-      ></f7-list-item>
-      <f7-list-item
-        radio
-        name="demo-media-radio"
-        value="4"
-        title="John Doe (via Twitter)"
-        after="15:32"
-        subtitle="John Doe (@_johndoe) mentioned you on Twitter!"
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis tellus ut turpis condimentum, ut dignissim lacus tincidunt. Cras dolor metus, ultrices condimentum sodales sit amet, pharetra sodales eros. Phasellus vel felis tellus. Mauris rutrum ligula nec dapibus feugiat. In vel dui laoreet, commodo augue id, pulvinar lacus."
-      ></f7-list-item>
-    </f7-list>
+        <f7-list-item
+          radio
+          checked
+          name="demo-media-radio"
+          value="1"
+          title="Facebook"
+          after="17:14"
+          subtitle="New messages from John Doe"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis tellus ut turpis condimentum, ut dignissim lacus tincidunt. Cras dolor metus, ultrices condimentum sodales sit amet, pharetra sodales eros. Phasellus vel felis tellus. Mauris rutrum ligula nec dapibus feugiat. In vel dui laoreet, commodo augue id, pulvinar lacus."
+        ></f7-list-item>
+        <f7-list-item
+          radio
+          name="demo-media-radio"
+          value="2"
+          title="John Doe (via Twitter)"
+          after="17:11"
+          subtitle="John Doe (@_johndoe) mentioned you on Twitter!"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis tellus ut turpis condimentum, ut dignissim lacus tincidunt. Cras dolor metus, ultrices condimentum sodales sit amet, pharetra sodales eros. Phasellus vel felis tellus. Mauris rutrum ligula nec dapibus feugiat. In vel dui laoreet, commodo augue id, pulvinar lacus."
+        ></f7-list-item>
+        <f7-list-item
+          radio
+          name="demo-media-radio"
+          value="3"
+          title="Facebook"
+          after="16:48"
+          subtitle="New messages from John Doe"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis tellus ut turpis condimentum, ut dignissim lacus tincidunt. Cras dolor metus, ultrices condimentum sodales sit amet, pharetra sodales eros. Phasellus vel felis tellus. Mauris rutrum ligula nec dapibus feugiat. In vel dui laoreet, commodo augue id, pulvinar lacus."
+        ></f7-list-item>
+        <f7-list-item
+          radio
+          name="demo-media-radio"
+          value="4"
+          title="John Doe (via Twitter)"
+          after="15:32"
+          subtitle="John Doe (@_johndoe) mentioned you on Twitter!"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis tellus ut turpis condimentum, ut dignissim lacus tincidunt. Cras dolor metus, ultrices condimentum sodales sit amet, pharetra sodales eros. Phasellus vel felis tellus. Mauris rutrum ligula nec dapibus feugiat. In vel dui laoreet, commodo augue id, pulvinar lacus."
+        ></f7-list-item>
+      </f7-list>
   </f7-page>
 </template>
 <script>
