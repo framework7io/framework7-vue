3c6cf48d9eab7e41b26382dc764d7c2cbfa147df
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-10-10 19:09:31 +0300

    Color mixins

--- src/utils/mixins.js
+++ src/utils/mixins.js
@@ -1,6 +1,32 @@
 import Utils from './utils';
 
 const Mixins = {
+  colorProps: {
+    color: String,
+    colorTheme: String,
+    textColor: String,
+    bgColor: String,
+    borderColor: String,
+    rippleColor: String,
+  },
+  colorClasses(self) {
+    const {
+      color,
+      colorTheme,
+      textColor,
+      bgColor,
+      borderColor,
+      rippleColor,
+    } = self;
+    return {
+      [`color-${color}`]: color,
+      [`color-theme-${colorTheme}`]: colorTheme,
+      [`text-color-${textColor}`]: textColor,
+      [`bg-color-${bgColor}`]: bgColor,
+      [`border-color-${borderColor}`]: borderColor,
+      [`ripple-color-${rippleColor}`]: rippleColor,
+    };
+  },
   linkIconProps: {
     icon: String,
     iconMaterial: String,
