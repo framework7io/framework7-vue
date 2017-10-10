94048ec115c60f4b08c2756ac2b97ac3e47f2a3a
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-10-10 19:09:25 +0300

    Same logic for hooks

--- src/utils/directives.js
+++ src/utils/directives.js
@@ -1,27 +1,15 @@
 const Directives = {};
 ['color', 'color-theme', 'text-color', 'bg-color', 'border-color', 'ripple-color'].forEach((name) => {
-  Directives[`f7-${name}`] = {
-    bind(el, binding) {
-      const { value, oldValue } = binding;
-      if (!el.classList) return;
-      if (!value) return;
-      if (oldValue) {
-        el.classList.remove(`${name}-${oldValue}`);
-      }
-      if (value) {
-        el.classList.add(`${name}-${value}`);
-      }
-    },
-    update(el, binding) {
-      const { value, oldValue } = binding;
-      if (value === oldValue) return;
-      if (oldValue) {
-        el.classList.remove(`${name}-${oldValue}`);
-      }
-      if (value) {
-        el.classList.add(`${name}-${value}`);
-      }
-    },
+  Directives[`f7-${name}`] = function f7ColorDirective(el, binding) {
+    const { value, oldValue } = binding;
+    if (value === oldValue) return;
+    if (!value && !oldValue) return;
+    if (oldValue) {
+      el.classList.remove(`${name}-${oldValue}`);
+    }
+    if (value) {
+      el.classList.add(`${name}-${value}`);
+    }
   };
 });
 
