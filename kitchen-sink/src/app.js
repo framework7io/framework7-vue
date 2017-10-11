e075643fb1251b652fe7d8717dcc09ebbd0de428
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-10-11 16:41:41 +0300

    Use bundle build

--- kitchen-sink/src/app.js
+++ kitchen-sink/src/app.js
@@ -1,6 +1,6 @@
 /* eslint import/no-extraneous-dependencies: "off" */
 import Vue from 'vue';
-import Framework7 from 'framework7';
+import Framework7 from 'framework7/dist/js/framework7.esm.bundle';
 import { Framework7Vue } from 'framework7-vue'; // eslint-disable-line
 import app from './app.vue';
 import routes from './routes';
