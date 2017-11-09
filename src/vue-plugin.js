57b3ce1d950d15fda956becc4725651c04487f75
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-11-09 20:06:52 +0300

    Route change events

--- src/vue-plugin.js
+++ src/vue-plugin.js
@@ -45,8 +45,19 @@ export default {
       eventHub.$emit('f7Ready', f7Instance);
     }
 
-    // Extend Router
-    Framework7.Router.use(VueRouter);
+    // Extend F7 Router
+    Framework7.Router
+      .use(VueRouter)
+      .use({
+        on: {
+          routeChange(to, from, router) {
+            eventHub.$emit('f7RouteChange', to, from, router);
+          },
+          routeChanged(to, from, router) {
+            eventHub.$emit('f7RouteChanged', to, from, router);
+          },
+        },
+      });
 
     // Mixin
     Vue.mixin({
@@ -78,16 +89,58 @@ export default {
           }
           parent = parent.$parent;
         }
+        if ($route && $router) {
+          self.$f7route = $route;
+          self.$f7router = $router;
+          self.$f7Route = $route;
+          self.$f7Router = $router;
+        }
+      },
+      beforeDestroy() {
+        const self = this;
+        if (self.$f7RouteChangeCallback) eventHub.$off('f7RouteChange', self.$f7RouteChangeCallback);
+        if (self.$f7RouteChangedCallback) eventHub.$off('f7RouteChanged', self.$f7RouteChangedCallback);
+      },
+      created() {
+        const self = this;
 
-        self.$f7route = $route;
-        self.$f7router = $router;
+        const routeChangeCallback = self.onF7RouteChange || self.F7RouteChange || self.f7RouteChange || self.f7routeChange;
+        const routeChangedCallback = self.onF7RouteChanged || self.F7RouteChanged || self.f7RouteChanged || self.f7routeChanged;
+        if (!routeChangeCallback && !routeChangedCallback) return;
+
+        function hasRouter(router) {
+          return (self.$f7router && router === self.$f7router) ||
+                 (!self.$f7Router && self.$f7 && self.$f7.router);
+        }
+
+        function addRoutesCallbacks() {
+          if (routeChangeCallback) {
+            self.$f7RouteChangeCallback = function onRouteChange(to, from, router) {
+              if (hasRouter(router)) {
+                routeChangeCallback(to, from, router);
+              }
+            };
+            eventHub.$on('f7RouteChange', self.$f7RouteChangeCallback);
+          }
+          if (routeChangedCallback) {
+            self.$f7RouteChangedCallback = function onRouteChanged(to, from, router) {
+              if (hasRouter(router)) {
+                routeChangedCallback(to, from, router);
+              }
+            };
+            eventHub.$on('f7RouteChanged', self.$f7RouteChangedCallback);
+          }
+        }
+
+        if (!self.$f7) eventHub.$on('f7Ready', addRoutesCallbacks);
+        else addRoutesCallbacks();
       },
       mounted() {
         const self = this;
         if (self === self.$root) {
           initFramework7(self.$root.$el, self.$options.framework7, self.$options.routes);
         }
-        const callback = self.onF7Ready || self.onF7ready || self.onF7Init || self.onF7init || self.f7Ready || self.f7Init;
+        const callback = self.onF7Ready || self.onF7ready || self.onF7Init || self.onF7init || self.f7Ready || self.f7Init || self.f7ready || self.f7init;
         if (!callback) return;
         if (f7Ready) callback(f7Instance);
         else {
