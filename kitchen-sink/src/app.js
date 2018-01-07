e2d241a69bce0d64033c81880caf7a76cdf52945
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-01-07 18:32:14 +0300

    Dynamic theme

--- kitchen-sink/src/app.js
+++ kitchen-sink/src/app.js
@@ -8,6 +8,12 @@ import routes from './routes';
 // Install Plugin
 Vue.use(Framework7Vue, Framework7);
 
+// Demo Theme
+let theme = 'auto';
+if (document.location.search.indexOf('theme=') >= 0) {
+  theme = document.location.search.split('theme=')[1].split('&')[0];
+}
+
 // Init Vue App
 export default new Vue({
   // Root Element
@@ -15,6 +21,10 @@ export default new Vue({
   render: c => c('app'),
   components: {
     app,
+  },
+  framework7: {
+    id: 'io.framework7.testapp',
+    theme,
   },
   routes,
 });
