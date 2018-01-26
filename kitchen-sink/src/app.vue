0bb963594b56c65725f3b44e50c1c484fe11e8fe
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-01-26 13:56:29 +0300

    Close tags

--- kitchen-sink/src/app.vue
+++ kitchen-sink/src/app.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <f7-statusbar></f7-statusbar>
     <f7-panel left cover>
-      <f7-view url="/panel-left/" links-view=".view-main" />
+      <f7-view url="/panel-left/" links-view=".view-main"></f7-view>
     </f7-panel>
     <f7-panel right reveal>
       <f7-view url="/panel-right/"></f7-view>
