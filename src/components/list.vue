f579ec0ce1fcccc659d2c09244ec128cff502a04
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-01-27 12:31:21 +0300

    remove unused prop

--- src/components/list.vue
+++ src/components/list.vue
@@ -7,7 +7,6 @@
       inset: Boolean,
       tabletInset: Boolean,
       mediaList: Boolean,
-      grouped: Boolean,
       sortable: Boolean,
       sortableEnabled: Boolean,
       accordionList: Boolean,
